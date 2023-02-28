142336dc73cdcd38f240d1ed8c4c9fb19a87bac0
sumeyyetuzuner17 <sumeyyedeniz34@gmail.com>
2023-02-28 07:52:20 +0300

    knex sorguları yazıldı(2 eksik)

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,13 +1,20 @@
 const getAll = () => {
   // KODLAR BURAYA
+   //select * from accounts
+   return db("accounts");
 }
 
 const getById = id => {
   // KODLAR BURAYA
+  //select * from accounts where id=id
+  return db("accounts").where({id}).first();
 }
 
 const create = account => {
   // KODLAR BURAYA
+    //insert into account values (id,name,budget)
+    
+
 }
 
 const updateById = (id, account) => {
@@ -16,6 +23,10 @@ const updateById = (id, account) => {
 
 const deleteById = id => {
   // KODLAR BURAYA
+    //delete from accounts where id=id
+    return db('accounts')
+    .where('id', id)
+    .del();
 }
 
 module.exports = {
